5cad80af8e87c5062b9ff086275aaa5abeed3cdf
emblica bigrig <bigrig@emblica.com>
2022-01-04 23:41:51 +0200

    Small fixes to predict.py

--- predict.py
+++ predict.py
@@ -42,63 +42,57 @@ num_labels = len(label_rgbs.keys())
 class_threshold = 0.8
 min_bounding_box_size = 1000 # Theoretical max for 480x480 is 230400 pixels
 
-def printr(value):
-    print(' '*200, end="")
-    print('\r', end="")
-    print(value, end="")
-    print('\r', end="")
-
 class Equirectangular:
     def __init__(self, img_name):
         self._img = cv2.imread(img_name, cv2.IMREAD_COLOR)
         [self._height, self._width, _] = self._img.shape
     
 
-def GetPerspective(self, FOV=0, THETA=0, PHI=0, height=0, width=0):
-    #
-    # THETA is left/right angle, PHI is up/down angle, both in degree
-    #
+    def GetPerspective(self, FOV=0, THETA=0, PHI=0, height=0, width=0):
+        #
+        # THETA is left/right angle, PHI is up/down angle, both in degree
+        #
 
-    equ_h = self._height
-    equ_w = self._width
-    equ_cx = (equ_w - 1) / 2.0
-    equ_cy = (equ_h - 1) / 2.0
+        equ_h = self._height
+        equ_w = self._width
+        equ_cx = (equ_w - 1) / 2.0
+        equ_cy = (equ_h - 1) / 2.0
 
-    wFOV = FOV
-    hFOV = float(height) / width * wFOV
+        wFOV = FOV
+        hFOV = float(height) / width * wFOV
 
-    w_len = np.tan(np.radians(wFOV / 2.0))
-    h_len = np.tan(np.radians(hFOV / 2.0))
-
-
-    x_map = np.ones([height, width], np.float32)
-    y_map = np.tile(np.linspace(-w_len, w_len,width), [height,1])
-    z_map = -np.tile(np.linspace(-h_len, h_len,height), [width,1]).T
-
-    D = np.sqrt(x_map**2 + y_map**2 + z_map**2)
-    xyz = np.stack((x_map,y_map,z_map),axis=2)/np.repeat(D[:, :, np.newaxis], 3, axis=2)
-
-    y_axis = np.array([0.0, 1.0, 0.0], np.float32)
-    z_axis = np.array([0.0, 0.0, 1.0], np.float32)
-    [R1, _] = cv2.Rodrigues(z_axis * np.radians(THETA))
-    [R2, _] = cv2.Rodrigues(np.dot(R1, y_axis) * np.radians(-PHI))
-
-    xyz = xyz.reshape([height * width, 3]).T
-    xyz = np.dot(R1, xyz)
-    xyz = np.dot(R2, xyz).T
-    lat = np.arcsin(xyz[:, 2])
-    lon = np.arctan2(xyz[:, 1] , xyz[:, 0])
-
-    lon = lon.reshape([height, width]) / np.pi * 180
-    lat = -lat.reshape([height, width]) / np.pi * 180
-
-    lon = lon / 180 * equ_cx + equ_cx
-    lat = lat / 90  * equ_cy + equ_cy
+        w_len = np.tan(np.radians(wFOV / 2.0))
+        h_len = np.tan(np.radians(hFOV / 2.0))
 
 
+        x_map = np.ones([height, width], np.float32)
+        y_map = np.tile(np.linspace(-w_len, w_len,width), [height,1])
+        z_map = -np.tile(np.linspace(-h_len, h_len,height), [width,1]).T
 
-    persp = cv2.remap(self._img, lon.astype(np.float32), lat.astype(np.float32), cv2.INTER_CUBIC, borderMode=cv2.BORDER_WRAP)
-    return persp
+        D = np.sqrt(x_map**2 + y_map**2 + z_map**2)
+        xyz = np.stack((x_map,y_map,z_map),axis=2)/np.repeat(D[:, :, np.newaxis], 3, axis=2)
+
+        y_axis = np.array([0.0, 1.0, 0.0], np.float32)
+        z_axis = np.array([0.0, 0.0, 1.0], np.float32)
+        [R1, _] = cv2.Rodrigues(z_axis * np.radians(THETA))
+        [R2, _] = cv2.Rodrigues(np.dot(R1, y_axis) * np.radians(-PHI))
+
+        xyz = xyz.reshape([height * width, 3]).T
+        xyz = np.dot(R1, xyz)
+        xyz = np.dot(R2, xyz).T
+        lat = np.arcsin(xyz[:, 2])
+        lon = np.arctan2(xyz[:, 1] , xyz[:, 0])
+
+        lon = lon.reshape([height, width]) / np.pi * 180
+        lat = -lat.reshape([height, width]) / np.pi * 180
+
+        lon = lon / 180 * equ_cx + equ_cx
+        lat = lat / 90  * equ_cy + equ_cy
+
+
+
+        persp = cv2.remap(self._img, lon.astype(np.float32), lat.astype(np.float32), cv2.INTER_CUBIC, borderMode=cv2.BORDER_WRAP)
+        return persp
 
     
 def perspective_correction(input_path, output_folder, steps=6):
@@ -223,7 +217,7 @@ def get_bounding_boxes(stiched_prediction_filepaths):
     return boxes
 
 def run_pipeline(target_filepath, output_folder='./temp'):
-    printr('Initializing...')
+    print('Initializing...')
     
     # create temp folder
     target_filepath_split = target_filepath.split("/")
@@ -242,21 +236,21 @@ def run_pipeline(target_filepath, output_folder='./temp'):
     filenames_digits = len(str(filenames_n))
     
     print(f'Total number of files: {filenames_n}')
-    printr(f'Done: {"0".zfill(filenames_digits)}/{filenames_n}')
+    print(f'Done: {"0".zfill(filenames_digits)}/{filenames_n}')
     """
     # The Pipeline
-    printr(f'Correcting perspective...')
+    print(f'Correcting perspective...')
     perspective_corrected_filepaths = perspective_correction(target_filepath, output_folder)
-    printr(f'Creating patches...')
+    print(f'Creating patches...')
     patched_npz_filepaths = create_patches(perspective_corrected_filepaths)
-    printr(f'Running prediction...')
+    print(f'Running prediction...')
     prediction_filepaths = predict_with(model, patched_npz_filepaths)
-    printr(f'Stitching prediction back...')
+    print(f'Stitching prediction back...')
     stiched_back_filepaths = stich_back(perspective_corrected_filepaths)
-    printr(f'Calculating degrees...')
+    print(f'Calculating degrees...')
     degrees = get_bounding_boxes(stiched_back_filepaths)
     with open(f'{output_folder}/results.json', 'w+') as outfile:
         json.dump(degrees, outfile)
-    printr('Done!')
+    print('Done!')
 
 run_pipeline(target, outfolder)
